f49db8c28a01c19d49ccf0bad689a7589f3e0f8b
Titova-Anastasiya <110681363+Stasya333@users.noreply.github.com>
2023-06-01 18:16:34 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -2,17 +2,22 @@
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
+
 class Train {
  private:
   struct Cage {
+    Cage():light(false), next(nullptr), prev(nullptr) {}
     bool light; // состояние лампочки
     Cage *next;
     Cage *prev;
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Cage *first; // точка входа в поезд (первый вагон)
+  int amount;
+  Cage *head; // точка входа в поезд (первый вагон)
+  Cage *tail;
+
  public:
-  Train();
+  Train():countOp(0), amount(0), head(nullptr), tail(nullptr) {}
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
